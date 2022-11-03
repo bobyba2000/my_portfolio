5d6b72bb78d1b50ea49842ef78ba5bdd66643202
Tran Tuan Dat <bobyba20@gmail.com>
2022-11-03 21:27:22 +0700

    fix bug'

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,6 +115,49 @@ function changeFeedback(userFeedbackDB){
     var listFeedback = listFeedbackDB.map(e => generateFeedbackItem(e['title'], e['subTitle'], e['content'], e['video']))
     userFeedback.innerHTML = '';
     userFeedback.replaceChildren(...listFeedback)
+
+    var s           = $('.feedback-slider'),
+          sWrapper    = s.find('.feedback-slider-wrapper'),
+          sItem       = s.find('.feedback-slide'),
+          btn         = s.find('.feedback-slider-link'),
+          sWidth      = sItem.width(),
+          sCount      = sItem.length,
+          slide_date  = s.find('.feedback-slide-date'),
+          slide_title = s.find('.feedback-slide-title'),
+          slide_text  = s.find('.feedback-slide-text'),
+          slide_more  = s.find('.feedback-slide-more'),
+          slide_image = s.find('.feedback-slide-image img'),
+          sTotalWidth = sCount * sWidth;
+      
+      sWrapper.css('width', sTotalWidth);
+      sWrapper.css('width', sTotalWidth);
+      
+      var clickCount  = 0;
+      
+      btn.on('click', function(e) {
+        e.preventDefault();
+  
+        if( $(this).hasClass('next') ) {
+          
+          ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
+        } else if ( $(this).hasClass('prev') ) {
+          
+          ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
+        }
+        TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+  
+  
+        //CONTENT ANIMATIONS
+  
+        var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
+        var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
+  
+        TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
+        TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
+        TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
+        TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
+        TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
+    })
 }
 
 function changeAbout(userAboutDB) {
@@ -574,56 +617,7 @@ function sendEmail() {
     $('.counter').counterUp({ delay: 10, time: 1600, }); $('.image-popup').magnificPopup({ type: 'image', gallery: { enabled: true } }); $(window).on('scroll', function (event) { if ($(this).scrollTop() > 600) { $('.back-to-top').fadeIn(200) } else { $('.back-to-top').fadeOut(200) } }); $('.back-to-top').on('click', function (event) { event.preventDefault(); $('html, body').animate({ scrollTop: 0, }, 1500); });
 }(jQuery));
 
-( function($) {
-  
-    $(document).ready(function() {
-      
-      var s           = $('.feedback-slider'),
-          sWrapper    = s.find('.feedback-slider-wrapper'),
-          sItem       = s.find('.feedback-slide'),
-          btn         = s.find('.feedback-slider-link'),
-          sWidth      = sItem.width(),
-          sCount      = sItem.length,
-          slide_date  = s.find('.feedback-slide-date'),
-          slide_title = s.find('.feedback-slide-title'),
-          slide_text  = s.find('.feedback-slide-text'),
-          slide_more  = s.find('.feedback-slide-more'),
-          slide_image = s.find('.feedback-slide-image img'),
-          sTotalWidth = sCount * sWidth;
-      
-      sWrapper.css('width', sTotalWidth);
-      sWrapper.css('width', sTotalWidth);
-      
-      var clickCount  = 0;
-      
-      btn.on('click', function(e) {
-        e.preventDefault();
-  
-        if( $(this).hasClass('next') ) {
-          
-          ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
-        } else if ( $(this).hasClass('prev') ) {
-          
-          ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
-        }
-        TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
-  
-  
-        //CONTENT ANIMATIONS
-  
-        var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
-        var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
-  
-        TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
-        TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
-        TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
-        TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
-        TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
-  
-      });
-            
-    });
-  })(jQuery);
+
   
   $('.overlay').addClass('overlay-blue');
 
